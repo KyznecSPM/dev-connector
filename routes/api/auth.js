52c5a42396b603df6f2de6ccec9a5a32d5ef9df3
KyznecSPM <eme.di@ya.ru>
2019-09-30 23:14:41 +0300

    add login, logout with redirect

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -34,7 +34,8 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
 
     const { email, password } = req.body;
 
